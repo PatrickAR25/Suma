967c88cd9510e6b65f609d1fa1b6be092e7a5020
LAB_F402 <labf402@continental.edu.pe>
2024-09-02 15:16:22 -0500

    Inicio del Proyecto

---
+++ Suma_v1_2.py
@@ -0,0 +1,13 @@
+class OperacionesAritmeticas:
+    def calcularSuma(sumando1, sumando2):
+        return sumando1 + sumando2
+
+    def leerSumandos():
+        sumando1 = int(input("Primer sumando: "))
+        sumando2 = int(input("Segundo sumando: "))
+        return sumando1 + sumando2
+
+    if __name__ := '__main__':
+        operacion = OperacionesAritmeticas()
+        sumando1, sumando2 = leerSumandos()
+        print(f"{sumando1} + {sumando2} = {operacion.calcularSuma(sumando1, sumando2)}")
